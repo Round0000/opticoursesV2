d1d7e5b4832ab5fd07eee07220fdbf5f542f36b5
Round0000 <billybinche@gmail.com>
2023-01-07 13:18:45 +0100

    commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -36,4 +36,17 @@ export class AppComponent {
   onError(e: any) {
     console.log(e)
   }
+
+  ngOnInit() {
+    if (!("BarcodeDetector" in window)) {
+      console.log("Barcode Detector is not supported by this browser.");
+    } else {
+      console.log("Barcode Detector supported!");
+
+      // create new detector
+      // const barcodeDetector = new BarcodeDetector({
+      //   formats: ["code_39", "codabar", "ean_13"],
+      // });
+    }
+  }
 }
